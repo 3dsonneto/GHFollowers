f1f1b403c85eb24b85e49beb1251e8b8187d3d88
Edson <edson_neto1996@hotmail.com>
2021-06-28 21:53:24 -0300

    Created search and favorites view controller and decluttered the scene delegate

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Edson Pessoal on 28/06/21.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Edson Pessoal on 28/06/21.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+
+}
