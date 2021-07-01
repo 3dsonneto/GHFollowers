ed2ed54f873df4d5f46171853e5a5b7348f701b0
Edson <edson_neto1996@hotmail.com>
2021-07-01 19:59:11 -0300

    Implemented the alert container view

--- GHFollowers/Custom Views/GFAlertContainerView.swift
+++ GHFollowers/Custom Views/GFAlertContainerView.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class GFAlertContainerView: UIViewController {
+class GFAlertContainerView: UIView {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
     
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure(){
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
