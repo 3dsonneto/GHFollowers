dd6438bf9f288713bc9f2a665c9796b32d654409
Edson <edson_neto1996@hotmail.com>
2021-07-01 19:38:25 -0300

    Rearranged files and created a Follower model

---
+++ GHFollowers/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,19 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Edson Pessoal on 30/06/21.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- GHFollowers/Extensions/UIViewController+Ext.swift
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-extension UIViewController{
+extension UIViewController{ //foi criado uma extension porque queremos que todos os alerts com esse comportamento
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String){ //nao pode chamar elemento do background thread
         DispatchQueue.main.async {
             let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
             alertVC.modalPresentationStyle  = .overFullScreen
-            alertVC.modalTransitionStyle    = .crossDissolve
+            alertVC.modalTransitionStyle    = .crossDissolve //fade para aparecer e sumir
             self.present(alertVC, animated: true)
         }
     }
