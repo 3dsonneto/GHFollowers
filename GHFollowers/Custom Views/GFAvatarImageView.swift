c80cb6c7451dec12457cbde662324954cdbf8c43
Edson <edson_neto1996@hotmail.com>
2021-07-13 20:17:58 -0300

    Configure the collection view and created the three column flow layout

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,36 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Edson Pessoal on 12/07/21.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")! //como esta nos assets ta garantido que não vai ser nil
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure(){
+        layer.cornerRadius  = 10 //borda arredondada
+        clipsToBounds       = true //precisa para cortar a borda da imagem também
+        image               = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        
+    }
+
+}
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,14 +10,55 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+
+    }
+    
+    
+    func configureViewController(){
         view.backgroundColor                                    = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles  = true
+    }
+    
+    
+    func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout()) //view.bounds preenche a view toda | flow layout é a estilização da collection view
+        view.addSubview(collectionView)// primeiro inicializa(acima) depois utiliza senão ele vem vazio
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width //largura total da tela
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40) //altura + espaco da label
         
         
         
+        return flowLayout
+    }
+
+    
+    func getFollowers(){
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
@@ -29,14 +70,6 @@ class FollowerListVC: UIViewController {
             }
     
         }
-        
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
-
-    }
-
 
 }
