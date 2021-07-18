a3e103e16e4012e5755fa4effe14c6e4ab032be7
Edson <edson_neto1996@hotmail.com>
2021-07-18 18:03:06 -0300

    Implemented the search functionality

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -15,6 +15,7 @@ class FollowerListVC: UIViewController {
     
     var username: String!
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
     
@@ -23,6 +24,7 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
@@ -51,7 +53,14 @@ class FollowerListVC: UIViewController {
     }
     
     
-    
+    func configureSearchController(){
+        let searchController                    = UISearchController()
+        searchController.searchResultsUpdater   = self //toda vez que o resultado de busca ele avisa
+        searchController.searchBar.delegate     = self
+        searchController.searchBar.placeholder  = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false //tira o tint da view embaixo de quando voce toca na barra de busca
+        navigationItem.searchController         = searchController
+    }
 
     
     func getFollowers(username: String, page: Int){
@@ -71,7 +80,7 @@ class FollowerListVC: UIViewController {
                     }
                     return
                 }
-                self.updateData()
+                self.updateData(on: self.followers)
             
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok") //rawvalue valor do erro no enum
@@ -90,7 +99,7 @@ class FollowerListVC: UIViewController {
     }
     
     
-    func updateData(){
+    func updateData(on followers: [Follower]){
         var snapshop = NSDiffableDataSourceSnapshot<Section, Follower>() //snapshot dos dados antes e depois(faz parte do funcionamento do diffabledatasource)
         snapshop.appendSections([.main]) //so tem uma section, por isso o array so tem um elemento(main)
         snapshop.appendItems(followers)
@@ -112,4 +121,20 @@ extension FollowerListVC: UICollectionViewDelegate{
             getFollowers(username: username, page: page)
         }
     }
+}
+
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate{
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+         
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) } //$0 é o item que eu to(o filter percorre o array), aí checa o login minusculo e vê se contem o que tem no filtro e joga pro filteredFollowers que so tem as coias que contem o filtro
+        updateData(on: filteredFollowers)
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
+    }
+    
 }
