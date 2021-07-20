43668657e70a6fb8b2c5490f00e0fea92b5dd971
Edson <edson_neto1996@hotmail.com>
2021-07-20 16:56:49 -0300

    Created the secondary label

---
+++ GHFollowers/Custom Views/ViewControllers/GFUserHeaderVC.swift
@@ -0,0 +1,19 @@
+//
+//  GFUserHeaderVC.swift
+//  GHFollowers
+//
+//  Created by Edson Pessoal on 20/07/21.
+//
+
+import UIKit
+
+class GFUserHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
