edcc09cf20ace94364c2d617de5a8bc35e96e891
Edson <edson_neto1996@hotmail.com>
2021-07-26 14:57:33 -0300

    Implemented the persistence manager

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -12,4 +12,6 @@ enum GFError: String, Error { //string é raw value(todos os cases conformam a e
     case unableToComplete   = "Unable to complete your request, please check your internet connection."
     case invalidResponse    = "Invalid response from the server. Please try again."
     case invalidData        = "The data received from the server was invalid. Please try again."
+    case unableToFavorite   = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You've already favorited this user. You must REALLY like them."
 }
