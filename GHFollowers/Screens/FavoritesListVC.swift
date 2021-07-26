846322673297f2745d236c606506bf542ba22a03
Edson <edson_neto1996@hotmail.com>
2021-07-26 16:31:02 -0300

    Implemented the swipe to delete on the favorites list

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -71,8 +71,6 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
         return favorites.count
     }
     
-    
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID) as! FavoriteCell
         let favorite = favorites[indexPath.row]
@@ -89,4 +87,19 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
         navigationController?.pushViewController(destVC, animated: true)
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistanceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            
+            guard let error = error else { return }
+            
+            self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+    }
+}
 }
